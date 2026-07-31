d1d4c1443e5291197d3f5b68a3e70b2f197d0c4e
Ben <meowcat@meowcat.site>
2026-07-31 11:05:51 +0100

    working on android builds

--- src/main/java/site/meowcat/Interfaces/GameMode.java
+++
@@ -1,7 +0,0 @@
-package site.meowcat.Interfaces;
-
-public interface GameMode {
-    void onDigitHit(int digit);
-
-    String getHudText(int nextDigit);
-}
--- src/main/java/site/meowcat/managers/GameSettings.java
+++
@@ -1,7 +0,0 @@
-package site.meowcat.managers;
-
-public class GameSettings {
-    public static float volume = 0.8f;
-    public static float hitWindow = 0.25f;
-    public static boolean fullscreen = false;
-}
